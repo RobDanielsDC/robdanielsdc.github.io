dadf5b81bdaa4583c84c68418058c1a29b45eed5
RobDanielsDC <robdaniels.design@gmail.com>
2023-04-14 16:14:19 -0400

    re-starting my portfolio for the last time
    sike

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-const breakpoint = window.matchMedia('(max-width: 768px)');
+const section = document.querySelector('.text-scroll');
+const leftLine = document.querySelector('.left');
+const rightLine = document.querySelector('.right');
+const sectionTop = section.offsetTop;
+const sectionHeight = section.offsetHeight;
+const scrollSpeed = 1; /* adjust the scroll speed as needed */
 
-if (breakpoint.matches) {
-    const marquee = document.querySelector('.marquee');
-    const logos = marquee.querySelectorAll('img');
-    
-    logos.forEach((logo) => {
-      const clone = logo.cloneNode(true);
-      marquee.appendChild(clone);
-    });
-    
-    console.log('This code will only execute on screens smaller than 768 pixels');
-}
+window.addEventListener('scroll', () => {
+  const scrollTop = window.pageYOffset;
+  const scrollBottom = scrollTop + window.innerHeight;
 
+  /* check if the section is in view */
+  if (scrollBottom > sectionTop && scrollTop < sectionTop + sectionHeight) {
+    /* calculate the scroll distance from the top of the section */
+    const scrollDistance = (scrollBottom - sectionTop) / sectionHeight;
+    
+    /* update the transform property of the lines of text */
+    leftLine.style.transform = `translateX(${scrollDistance * 100}%)`;
+    rightLine.style.transform = `translateX(-${scrollDistance * 100}%)`;
+  } else {
+    /* reset the transform property of the lines of text */
+    leftLine.style.transform = 'translateX(-100%)';
+    rightLine.style.transform = 'translateX(100%)';
+  }
+});
